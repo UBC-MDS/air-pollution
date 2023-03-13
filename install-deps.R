2f250a6fdde8c332d4c15c656fc8b9a98166dd41
netsgnut <284779+netsgnut@users.noreply.github.com>
2023-03-13 13:06:03 -0700

    cd: add a GitHub Actions to deploy to shinyapps

--- install-deps.R
+++ install-deps.R
@@ -1,4 +1,4 @@
 install.packages(
-  c("shiny", "fmsb", "tidyverse", "plyr", "leaflet"),
+  c("shiny", "fmsb", "tidyverse", "plyr", "leaflet", "rsconnect"),
   repos = c("https://cloud.r-project.org/")
 )
