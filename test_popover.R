2066f62403715acbe4c58e4b76e783138439d06a
Ritisha Sharma <ritishas2000@gmail.com>
2023-03-16 16:44:01 -0700

    Add description of pollutants

--- test_popover.R
+++ test_popover.R
@@ -37,31 +37,31 @@ pollutant_description <- c(
                 "At a level of 100 ppm, it is very hazardous to health.<br>",
                 "<br><small>Source: https://en.wikipedia.org/wiki/Nitric_oxide</small>"),
   "NO2" = paste0("Nitrogen Dioxide can be a result of vehicles burning fuel. <br>",
-                "Usually, exposure to NO2 causes harm slowly. <br>",
-                "The results could be mild irritation of the nose and throat. <br>",
-                "At higher levels, NO2 could lead to lung issues and even death. <br>",
-                "<br><small>Source: https://en.wikipedia.org/wiki/Nitrogen_dioxide</small>"),
+                 "Usually, exposure to NO2 causes harm slowly. <br>",
+                 "The results could be mild irritation of the nose and throat. <br>",
+                 "At higher levels, NO2 could lead to lung issues and even death. <br>",
+                 "<br><small>Source: https://en.wikipedia.org/wiki/Nitrogen_dioxide</small>"),
   "NOX" = paste0("Nitrogen oxides could be a result of combustion or the electric discarge <br>",
-                "during thunderstorms.<br>",
-                "It is a reddish-brown gas with a biting odor <br>",
-                "and is one of the most prominent air pollutants. <br>",
-                "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
+                 "during thunderstorms.<br>",
+                 "It is a reddish-brown gas with a biting odor <br>",
+                 "and is one of the most prominent air pollutants. <br>",
+                 "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
   "O3" = paste0("Ozone, found in the stratosphere, is important in making up the ozone layer.<br>",
                 "It is a pollutant and results mostly from the burning of fossil fuels. <br>",
                 "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
   "SO2" = paste0("Sulfur dioxide is created by volcanoes and the waste of industries. <br>",
-                "Burning of coal and petroleum can result in sulfur dioxide. <br>",
-                "Its part in creating acidic rain can have worrying results on the environment. <br>",
-                "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
+                 "Burning of coal and petroleum can result in sulfur dioxide. <br>",
+                 "Its part in creating acidic rain can have worrying results on the environment. <br>",
+                 "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
   "PM2.5" = paste0("Particulate matter/particles are particles supspended in gas are miscroscopic. <br>",
-                  "PM2.5 has a diameter of 2.5 micrometers.<br>",
-                  "They can be created by volcanoes, dust storms, forest and grassland fires, living plants, and sea spray.<br>",
-                  "Humans contribute to an increase of PMs by burning fossil fuels.<br>",
-                  "Overexposure to particulate matter is hazardous to humans and could cause heart and lung diseases. <br>",
-                  "It can be harmful to people with asthma.<br>",
-                  "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
+                   "PM2.5 has a diameter of 2.5 micrometers.<br>",
+                   "They can be created by volcanoes, dust storms, forest and grassland fires, living plants, and sea spray.<br>",
+                   "Humans contribute to an increase of PMs by burning fossil fuels.<br>",
+                   "Overexposure to particulate matter is hazardous to humans and could cause heart and lung diseases. <br>",
+                   "It can be harmful to people with asthma.<br>",
+                   "<br><small>Source: https://en.wikipedia.org/wiki/Air_pollution#Pollutants</small>"),
   "PM10" = paste0("PM10 has a diameter of 10 micrometers. See description for PM2.5 for more information.")
-  )
+)
 
 pollutant.colors <-
   RColorBrewer::brewer.pal(length(pollutants), pollutant.color.palette)
@@ -120,7 +120,7 @@ ui <- fluidPage(
         max-width: 100%; }
         .popover-title { display: none; }
     ")),
-  tags$script(HTML("
+    tags$script(HTML("
      $(document).ready(function(){
        $('body').popover({
          selector: '[data-toggle=\"popover\"]',
@@ -130,7 +130,7 @@ ui <- fluidPage(
          container: 'body'
          });});
       ")
-  )),
+    )),
   
   # Header
   includeHTML("header.html"),
@@ -414,14 +414,14 @@ server <- function(input, output, session) {
   })
   
   output$pol_desc <- renderUI({
-      HTML(
-        paste0(
-          sprintf(
-            "<a href='#' data-toggle='popover' title='%s' data-placement='bottom' data-content=''>%s</a>",
-            pollutant_description, 
-            pollutants), "<br>"
-        )
+    HTML(
+      paste0(
+        sprintf(
+          "<a href='#' data-toggle='popover' title='%s' data-placement='bottom' data-content=''>%s</a>",
+          pollutant_description, 
+          pollutants), "<br>"
       )
+    )
     
   })
   
